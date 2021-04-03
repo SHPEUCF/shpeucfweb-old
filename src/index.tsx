777bf14517f61737a1798b3f47aace76e7e2f2e6
idelmr <idel_martinez@knights.ucf.edu>
2021-04-03 19:11:31 -0400

    Add router and SemanticUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Router from './router';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home } from 'pages';
 
 export default () => (
 	<BrowserRouter>
 		<Switch>
-			<Route exact path = '/Home' component { Home } />
+			<Route exact path = '/' component = { Home } />
 		</Switch>
 	</BrowserRouter>
 );
